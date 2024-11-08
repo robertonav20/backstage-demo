9b2da8d09e2edf0ec392eb1c90053dccd816af8b
robertonav20 <roberto.nav20@gmail.com>
2024-11-08 01:21:58 +0100

    Add Api docs to spring boot service

--- tools/gitlab/templates/spring-boot-service/content/src/main/java/${{values.packageNameFolder}}/controller/ApiController.java
+++ tools/gitlab/templates/spring-boot-service/content/src/main/java/${{values.packageNameFolder}}/controller/ApiController.java
@@ -2,10 +2,12 @@ package ${{ values.groupId }}.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("${{ values.apiPrefix }}")
 public class ApiController {

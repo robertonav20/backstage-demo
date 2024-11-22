af459502aa301874cb9f5bbce2460be516ca72d6
robertonav20 <roberto.nav20@gmail.com>
2024-11-23 00:23:40 +0100

    Add api call inside hello app

--- tools/gitlab/templates/hello-world-app/skeleton/src/App.js
+++ tools/gitlab/templates/hello-world-app/skeleton/src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/v1/hello')
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          { data ? <pre>{data}</pre> : 'Loading...'}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
